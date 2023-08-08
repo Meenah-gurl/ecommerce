83ef83081030bf87cd6f62c7b1577ee00e51a723
meenah-gurl <aminasani.as64@gmail.com>
2023-08-08 16:15:43 +0100

    first commit

---
+++ mina-mart/src/App.js
@@ -0,0 +1,19 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='bg-green-900'>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+          Learn React
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
